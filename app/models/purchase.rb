1a9da24fd0c17c291182ee537a83f9e93117debc
John Pfingst <jpfingst@outlook.com>
2017-03-04 15:41:07 +0000

    update index page with a slogan

---
+++ app/models/purchase.rb
@@ -0,0 +1,5 @@
+class Purchase < ActiveRecord::Base
+#  belongs_to :user
+#  has_many :purchase_items
+#  has_many :items, through: purchase_item
+end
---
+++ app/models/purchase_item.rb
@@ -0,0 +1,5 @@
+class PurchaseItem < ActiveRecord::Base
+#  belongs_to :purchase
+#  belongs_to :item
+#  validates  :item_id, :sale_price, presence: true
+end
